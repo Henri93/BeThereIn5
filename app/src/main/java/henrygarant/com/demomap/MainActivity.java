1f98c730cae8be2f4e26d2d6b16fe6ddfe0c3ab3
Henry Garant <henryrgarant@gmail.com>
2015-11-10 21:51:22 -0500

    Add Starting Code

---
+++ app/src/main/java/henrygarant/com/demomap/MainActivity.java
@@ -0,0 +1,153 @@
+package henrygarant.com.demomap;
+
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+
+public class MainActivity extends FragmentActivity{
+
+    private Button searchButton;
+    private AutoCompleteTextView addressBar;
+    private String[] addressList = new String[]{"3030 Magee Ave"};
+    private ArrayAdapter<String> adapter;
+    private String query;
+    //private SearchAddress searchAddress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        getActionBar().setHomeButtonEnabled(true);
+
+        Typeface tf = Typeface.createFromAsset(getAssets(),"fonts/Exo-Regular.otf");
+
+        searchButton = (Button)findViewById(R.id.searchButton);
+        addressBar = (AutoCompleteTextView)findViewById(R.id.addressBar);
+
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, addressList);
+        adapter.setNotifyOnChange(true);
+        addressBar.setThreshold(2);
+        addressBar.setAdapter(adapter);
+
+        //searchAddress = new SearchAddress();
+        /*addressBar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                adapter.clear();
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if ((addressBar).isPerformingCompletion()) {return;}
+                if (s.length() < 2) {
+                    return;
+                }else{
+                    query = s.toString();
+                    if (searchAddress.getStatus().equals(AsyncTask.Status.FINISHED)){
+                        searchAddress = new SearchAddress();
+                        searchAddress.execute(query);
+                        Log.d("ASYNC", "FINISH GOOD");
+                        Log.d("ASYNC", query);
+                    }else{
+                        Log.d("ASYNC", "CANCEL");
+                        searchAddress.cancel(false);
+                        searchAddress = new SearchAddress();
+                        searchAddress.execute(query);
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                adapter.notifyDataSetChanged();
+            }
+        });*/
+
+
+        searchButton.setTypeface(tf, Typeface.BOLD);
+        addressBar.setTypeface(tf);
+    }
+
+
+    public void calculateLocation(View view)
+    {
+        new AlertDialog.Builder(this)
+                .setTitle("Set Notification")
+                .setMessage("Do you want to be reminded when they are within 5 minutes?")
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int whichButton){
+                        //TODO SET REMINDER HERE
+                        String address = addressBar.getText().toString().trim();
+                        moveToActivity(MapsActivity.class, address);
+                    }})
+                .setNegativeButton(android.R.string.no, null).show();
+    }
+
+    private void moveToActivity(Class newClass, String destination){
+        Intent intent = new Intent(this, newClass);
+        intent.putExtra("destination", destination);
+        startActivity(intent);
+    }
+
+
+
+    /*private ArrayList<String> getAddressInfo(Context context, String locationName){
+        Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+
+        try {
+            List<Address> a = geocoder.getFromLocationName(locationName, 5);
+
+            if(a != null) {
+
+                for (int i = 0; i < a.size(); i++) {
+                    //String city = a.get(i).getLocality();
+                    //String country = a.get(i).getCountryName();
+                    Address address = a.get(i);
+                    addressList.add(address.toString());
+                }
+            }
+            return addressList;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private class SearchAddress extends AsyncTask<String, Void, ArrayList<String>> {
+
+        @Override
+        protected ArrayList<String> doInBackground(String... params) {
+            addressList.clear();
+            ArrayList<String> addressArray = getAddressInfo(getApplicationContext(), query);
+            return addressArray;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<String> addressArray) {
+            if(addressArray == null)
+                Toast.makeText(getApplicationContext(), "No address obtained from server", Toast.LENGTH_SHORT).show();
+            else{
+                adapter.clear();
+                for(String address : addressArray){
+                    adapter.add(address);
+                }
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {}
+        @Override
+        protected void onProgressUpdate(Void... values) {}
+    }*/
+}
---
+++ app/src/main/java/henrygarant/com/demomap/MapsActivity.java
@@ -0,0 +1,126 @@
+package henrygarant.com.demomap;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity {
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private final int MILE_RADIUS = 1;
+    private int CIRCLE_COLOR =  Color.argb(100, 30, 136, 229);
+    private String destination;
+    private LatLng destinationLatLng = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        CIRCLE_COLOR =  getResources().getColor(R.color.Map_Color);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Intent intent = getIntent();
+        destination = intent.getStringExtra("destination");
+        makeDestinationLatLng(destination);
+        setUpMapIfNeeded();
+    }
+
+    private void makeDestinationLatLng(String destination) {
+        Geocoder coder = new Geocoder(this);
+        List<Address> address;
+        Address location = null;
+
+        try {
+            address = coder.getFromLocationName(destination, 1);
+            if (address == null) {
+                //TODO HANDLE NO LOCATION EVENT
+                return;
+            }
+            location = address.get(0);
+            destinationLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+        }catch (Exception e){
+
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera.
+     *
+     */
+    private void setUpMap() {
+
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
+        mMap.setMyLocationEnabled(true);
+
+
+        //LocationManager locManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        //Location currentLocation = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        // LatLng currentLatLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+
+
+        mMap.addMarker(new MarkerOptions()
+                .position(destinationLatLng)
+                .snippet("Your destination")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                .title("Destination"));
+
+        // Instantiates a new CircleOptions object and defines the center and radius
+        CircleOptions circleOptions = new CircleOptions()
+                .center(destinationLatLng)
+                .fillColor(CIRCLE_COLOR)
+                .strokeColor(CIRCLE_COLOR)
+                .radius(1609 * MILE_RADIUS);  //convert miles to meters
+
+        mMap.addCircle(circleOptions);
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                destinationLatLng, 13));
+    }
+}
