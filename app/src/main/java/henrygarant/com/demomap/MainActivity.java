d9c382e20d076286902869bf1b083db104caf405
Henry Garant <henryrgarant@outlook.com>
2016-01-01 17:51:33 -0500

    Back in 5 2

--- app/src/main/java/henrygarant/com/demomap/MainActivity.java
+++ app/src/main/java/henrygarant/com/demomap/MainActivity.java
@@ -59,6 +59,7 @@ public class MainActivity extends FragmentActivity{
                 .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener(){
                     public void onClick(DialogInterface dialog, int whichButton){
                         //TODO SET REMINDER HERE
+
                         String address = addressBar.getText().toString().trim()
                         moveToActivity(MapsActivity.class, address);
                     }})
