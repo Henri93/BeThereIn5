711e58a8bdfd624843a899927a2fc5c49a6d4dec
Henry Garant <henryrgarant@outlook.com>
2016-07-17 19:50:22 -0400

    Update MainActivity UI pics

--- app/src/main/java/henrygarant/com/demomap/ContactAdapter.java
+++ app/src/main/java/henrygarant/com/demomap/ContactAdapter.java
@@ -137,31 +137,4 @@ public class ContactAdapter extends BaseExpandableListAdapter {
 
 }
 
-    /*public ContactAdapter(Context context, ArrayList<Contact> contacts) {
-        super(context, 0 , contacts);
-
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        // Get the data item for this position
-        Contact contact = getItem(position);
-        // Check if an existing view is being reused, otherwise inflate the view
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.contact_adapter, parent, false);
-        }
-
-        Typeface font = Typeface.createFromAsset(getContext().getAssets(), "fonts/OpenSans-Light.ttf");
-        // Lookup view for data population
-        TextView contactName = (TextView) convertView.findViewById(R.id.contactName);
-        TextView contactNumber = (TextView) convertView.findViewById(R.id.contactNumber);
-        // Populate the data into the template view using the data object
-        contactName.setTypeface(font);
-        contactName.setTypeface(font);
-
-        contactName.setText(contact.getName());
-        contactNumber.setText(contact.getNumber());
-        // Return the completed view to render on screen
-        return convertView;
-    }*/
 
