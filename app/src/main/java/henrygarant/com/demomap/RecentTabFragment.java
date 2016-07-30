a8c40bfae8dfc23f501c8933a559549e969ef683
Henry Garant <henryrgarant@outlook.com>
2016-07-30 19:56:39 -0400

    add send activity

--- app/src/main/java/henrygarant/com/demomap/RecentTabFragment.java
+++ app/src/main/java/henrygarant/com/demomap/RecentTabFragment.java
@@ -19,7 +19,7 @@ public class RecentTabFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.recent, container, false);
+        View view = inflater.inflate(R.layout.send, container, false);
         sendPhone = (EditText)view.findViewById(R.id.sendPhone);
         sendText = (EditText)view.findViewById(R.id.sendText);
         sendButton = (Button)view.findViewById(R.id.sendButton);
