bb396bdcaf2e75f027bf626edd76c9e0ba953c20
Henry Garant <henryrgarant@outlook.com>
2016-07-30 20:31:57 -0400

    Add MainActivity menu functionality

--- app/src/main/java/henrygarant/com/demomap/MainActivity.java
+++ app/src/main/java/henrygarant/com/demomap/MainActivity.java
@@ -1,9 +1,12 @@
 package henrygarant.com.demomap;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -15,11 +18,25 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
+        getMenuInflater().inflate(R.menu.actionbarmenu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_new) {
+            //send to new activity
+            Intent intent = new Intent(this, SendActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
