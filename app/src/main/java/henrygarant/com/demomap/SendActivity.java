2b602f7512f8f8a03d4c0a08d5896a0308ec37ab
Henry Garant <henryrgarant@outlook.com>
2016-07-30 20:02:46 -0400

    add send activity

---
+++ app/src/main/java/henrygarant/com/demomap/SendActivity.java
@@ -0,0 +1,24 @@
+package henrygarant.com.demomap;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import henrygarant.com.demomap.GcmServices.GcmSender;
+
+public class RecentTabFragment extends ActionBarActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.send);
+    }
+}
